07333fbbeba6e3e4f4c631bef075d74604beaa49
limaxp <Maximilian.paar@gmx.at>
2022-05-24 20:28:01 +0200

    Update PostProcessKernel.java

--- src/org/jgine/render/shader/PostProcessKernel.java
+++ src/org/jgine/render/shader/PostProcessKernel.java
@@ -2,10 +2,14 @@ package org.jgine.render.shader;
 
 public class PostProcessKernel {
 
-	public static final float[] SHARPEN = new float[] { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
+	public static final float[] IDENTITY = { 0, 0, 0, 0, 1, 0, 0, 0, 0 };
 
-	public static final float[] BLUR = new float[] { 1.0f / 16, 2.0f / 16, 1.0f / 16, 2.0f / 16, 4.0f / 16, 2.0f / 16,
-			1.0f / 16, 2.0f / 16, 1.0f / 16 };
+	public static final float[] SHARPEN = { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
 
-	public static final float[] EDGE_DETECTION = new float[] { 1, 1, 1, 1, -8, 1, 1, 1, 1 };
+	public static final float[] BLUR = { 0.0625f, 0.125f, 0.0625f, 0.125f, 0.25f, 0.125f, 0.0625f, 0.125f, 0.0625f };
+
+	public static final float[] EDGE_DETECTION = { 1, 1, 1, 1, -8, 1, 1, 1, 1 };
+
+	public static final float[] BOX_BLUR = { 0.11111f, 0.11111f, 0.11111f, 0.11111f, 0.11111f, 0.11111f, 0.11111f,
+			0.11111f, 0.11111f };
 }
