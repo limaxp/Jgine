a364c5610960eb5c62c4760d0057046bc05f8c9e
limaxp <Maximilian.paar@gmx.at>
2022-10-21 13:48:03 +0200

    Update ParticleScene.java

--- src/org/jgine/system/systems/particle/ParticleScene.java
+++ src/org/jgine/system/systems/particle/ParticleScene.java
@@ -3,12 +3,9 @@ package org.jgine.system.systems.particle;
 import org.eclipse.jdt.annotation.Nullable;
 import org.jgine.core.Scene;
 import org.jgine.core.entity.Entity;
-import org.jgine.core.manager.SystemManager;
 import org.jgine.core.manager.TaskManager;
 import org.jgine.render.Renderer;
 import org.jgine.system.data.TransformListSystemScene;
-import org.jgine.system.systems.camera.Camera;
-import org.jgine.system.systems.camera.CameraSystem;
 
 public class ParticleScene extends TransformListSystemScene<ParticleSystem, ParticleObject> {
 
@@ -51,15 +48,11 @@ public class ParticleScene extends TransformListSystemScene<ParticleSystem, Part
 	public void render() {
 		Renderer.setShader(Renderer.PARTICLE_SHADER);
 		Renderer.enableDepthTest();
-
-		Camera camera = SystemManager.get(CameraSystem.class).getCamera();
-		Renderer.setCamera(camera);
 		for (int i = 0; i < size; i++) {
 			ParticleObject object = objects[i];
 			if (object.isActive())
 				Renderer.render(transforms[i].getMatrix(), object, object.material);
 		}
-
 		Renderer.disableDepthTest();
 	}
 }
