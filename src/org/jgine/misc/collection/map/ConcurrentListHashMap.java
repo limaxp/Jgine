b7521b14bd9879028d2945e21839e5d394ae06f0
limaxp <Maximilian.paar@gmx.at>
2022-11-07 12:07:07 +0100

    update Maps

--- src/org/jgine/misc/collection/map/ConcurrentListHashMap.java
+++ src/org/jgine/misc/collection/map/ConcurrentListHashMap.java
@@ -2,29 +2,30 @@ package org.jgine.misc.collection.map;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.eclipse.jdt.annotation.Nullable;
 import org.jgine.misc.collection.list.arrayList.FastArrayList;
 
-public class ArrayListHashMap<K, V> extends HashMap<K, List<V>> {
+public class ConcurrentListHashMap<K, V> extends ConcurrentHashMap<K, List<V>> {
 
-	private static final long serialVersionUID = 8092867687056028336L;
+	private static final long serialVersionUID = 4184018242529988778L;
 
-	public ArrayListHashMap() {
+	public ConcurrentListHashMap() {
 	}
 
-	public ArrayListHashMap(int initialCapacity) {
+	public ConcurrentListHashMap(int initialCapacity) {
 		super(initialCapacity);
 	}
 
-	public ArrayListHashMap(Map<K, List<V>> m) {
+	public ConcurrentListHashMap(Map<K, List<V>> m) {
 		super(m);
 	}
 
-	public ArrayListHashMap(int initialCapacity, float loadFactor) {
+	public ConcurrentListHashMap(int initialCapacity, float loadFactor) {
 		super(initialCapacity, loadFactor);
 	}
 
@@ -43,17 +44,21 @@ public class ArrayListHashMap<K, V> extends HashMap<K, List<V>> {
 
 	@Nullable
 	public K rem(V value) {
-		for (Entry<K, List<V>> entry : entrySet()) {
-			List<V> list = entry.getValue();
-			for (int i = 0; i < list.size(); i++) {
-				if (list.get(i).equals(value)) {
-					K key = entry.getKey();
-					get(key).remove(value);
-					return key;
+		K key = null;
+		synchronized (this) {
+			for (Entry<K, List<V>> entry : entrySet()) {
+				List<V> list = entry.getValue();
+				for (int i = 0; i < list.size(); i++) {
+					if (list.get(i).equals(value)) {
+						key = entry.getKey();
+						break;
+					}
 				}
 			}
 		}
-		return null;
+		if (key != null)
+			get(key).remove(value);
+		return key;
 	}
 
 	public void rem(K key, V value) {
@@ -76,9 +81,12 @@ public class ArrayListHashMap<K, V> extends HashMap<K, List<V>> {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<V> get(Object key) {
-		List<V> list = super.get(key);
-		if (list == null)
-			put((K) key, list = new FastArrayList<V>());
+		List<V> list;
+		synchronized (this) {
+			list = super.get(key);
+			if (list == null)
+				put((K) key, list = Collections.synchronizedList(new FastArrayList<V>()));
+		}
 		return list;
 	}
 
--- src/org/jgine/misc/collection/map/ListHashMap.java
+++ src/org/jgine/misc/collection/map/ListHashMap.java
@@ -2,30 +2,29 @@ package org.jgine.misc.collection.map;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.eclipse.jdt.annotation.Nullable;
 import org.jgine.misc.collection.list.arrayList.FastArrayList;
 
-public class ConcurrentArrayListHashMap<K, V> extends ConcurrentHashMap<K, List<V>> {
+public class ListHashMap<K, V> extends HashMap<K, List<V>> {
 
-	private static final long serialVersionUID = 4184018242529988778L;
+	private static final long serialVersionUID = 8092867687056028336L;
 
-	public ConcurrentArrayListHashMap() {
+	public ListHashMap() {
 	}
 
-	public ConcurrentArrayListHashMap(int initialCapacity) {
+	public ListHashMap(int initialCapacity) {
 		super(initialCapacity);
 	}
 
-	public ConcurrentArrayListHashMap(Map<K, List<V>> m) {
+	public ListHashMap(Map<K, List<V>> m) {
 		super(m);
 	}
 
-	public ConcurrentArrayListHashMap(int initialCapacity, float loadFactor) {
+	public ListHashMap(int initialCapacity, float loadFactor) {
 		super(initialCapacity, loadFactor);
 	}
 
@@ -44,21 +43,17 @@ public class ConcurrentArrayListHashMap<K, V> extends ConcurrentHashMap<K, List<
 
 	@Nullable
 	public K rem(V value) {
-		K key = null;
-		synchronized (this) {
-			for (Entry<K, List<V>> entry : entrySet()) {
-				List<V> list = entry.getValue();
-				for (int i = 0; i < list.size(); i++) {
-					if (list.get(i).equals(value)) {
-						key = entry.getKey();
-						break;
-					}
+		for (Entry<K, List<V>> entry : entrySet()) {
+			List<V> list = entry.getValue();
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i).equals(value)) {
+					K key = entry.getKey();
+					get(key).remove(value);
+					return key;
 				}
 			}
 		}
-		if (key != null)
-			get(key).remove(value);
-		return key;
+		return null;
 	}
 
 	public void rem(K key, V value) {
@@ -81,12 +76,9 @@ public class ConcurrentArrayListHashMap<K, V> extends ConcurrentHashMap<K, List<
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<V> get(Object key) {
-		List<V> list;
-		synchronized (this) {
-			list = super.get(key);
-			if (list == null)
-				put((K) key, list = Collections.synchronizedList(new FastArrayList<V>()));
-		}
+		List<V> list = super.get(key);
+		if (list == null)
+			put((K) key, list = new FastArrayList<V>());
 		return list;
 	}
 
